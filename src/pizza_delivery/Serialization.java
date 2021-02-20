9f9d513e2e446c6d1d81103ccdca4b1f7a3eb225
tony <tony_t_liu@outlook.com>
2021-02-20 21:21:54 +0800

    可以正常读取 Order 了

---
+++ src/pizza_delivery/Serialization.java
@@ -0,0 +1,52 @@
+
+package pizza_delivery;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Serialization {
+
+  public static Order parseOneFile(String filePath) {
+    List<String> list = new ArrayList<>();
+    try {
+      File file = new File(filePath);
+      if (file.isFile() && file.exists()) {
+        InputStreamReader read = new InputStreamReader(new FileInputStream(file));
+        BufferedReader bufferedReader = new BufferedReader(read);
+        String lineTxt = null;
+
+        while ((lineTxt = bufferedReader.readLine()) != null) {
+          list.add(lineTxt);
+        }
+        bufferedReader.close();
+        read.close();
+      }
+    } catch (Exception e) {
+      System.out.println("读取文件内容出错");
+      e.printStackTrace();
+    }
+    String readed_string = String.join("\n", list);
+    return Parser.parse(readed_string);
+  }
+
+    public static Order getA() {
+      return parseOneFile("data/a_example");
+    }
+
+    public static Order getB() {
+      return parseOneFile("data/b_little_bit_of_everything.in");
+    }
+
+    public static Order getC() {
+      return parseOneFile("data/c_many_ingredients.in");
+    }
+
+    public static Order getD() {
+      return parseOneFile("data/d_many_pizzas.in");
+    }
+
+    public static Order getE() {
+      return parseOneFile("data/e_many_teams.in");
+    }
+}
