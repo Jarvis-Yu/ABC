c8240a6d85f7cfff8789ff426e777db94da65dab
Yitang Chen <yc4120@ic.ac.uk>
2021-02-20 19:22:18 +0800

    parser good

--- src/Parser.java
+++ src/Parser.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.stream.Collectors;
 
 public class Parser {
 
@@ -10,7 +11,7 @@ public class Parser {
 		var line1 = lines[0].split(" ");
 		for (int i = 1; i < lines.length; i++) {
 			var lineN = lines[i].split(" ");
-			pizzas.add(new Pizza(new HashSet<String>(Arrays.asList(lineN))));
+			pizzas.add(new Pizza(new HashSet<String>(Arrays.stream(lineN).skip(1).collect(Collectors.toList()))));
 		}
 
 		return new Order(Integer.parseInt(line1[0]), Integer.parseInt(line1[1]), Integer.parseInt(line1[2]), Integer.parseInt(line1[3]), pizzas);
