997017adaa76a82f85c511846719e8cb111402d1
Alan (Jiacheng) Liang <alan.liang20@imperial.ac.uk>
2021-02-20 20:55:30 +0800

    update

--- src/GreedyAlgorithm.java
+++ src/GreedyAlgorithm.java
@@ -5,7 +5,9 @@ import java.util.TreeSet;
 
 public class GreedyAlgorithm {
 
-  public static final String t = """
+  public static int score = 0;
+
+  public static String t = """
 				5 1 2 1\s
 				3 onion pepper olive
 				3 mushroom tomato basil
@@ -14,6 +16,8 @@ public class GreedyAlgorithm {
 				2 chicken basil""";
 
   public static void solve(Order o ,int numberPerTeam, int teamNumber) {
+
+    t = System.in.toString();
 
     while(teamNumber > 0 && o.pizza.size() > numberPerTeam) {
       teamNumber -= 1;
@@ -32,7 +36,7 @@ public class GreedyAlgorithm {
         }
         Pizza bestPizza = o.pizza.remove(bestPizzaIndex);
         answerPizzas.add(bestPizza);
-        originalPizza.merge(bestPizza);
+        originalPizza = originalPizza.merge(bestPizza);
       }
 
       System.out.print(numberPerTeam + " ");
@@ -40,6 +44,7 @@ public class GreedyAlgorithm {
         System.out.print(pizzaUsed.getNo() + " ");
       }
       System.out.println();
+      score += Math.pow(originalPizza.getSize(), 2);
 
     }
   }
@@ -49,9 +54,16 @@ public class GreedyAlgorithm {
     var o = Parser.parse(t);
     o.pizza.sort(Pizza::compareTo);
 
+    int nof4 = o.numberOfPizza/4;
+    int nof3 = (o.numberOfPizza - nof4 * 4) / 3;
+    int nof2 = (o.numberOfPizza - nof4 * 4 - nof3 * 3) / 2;
+    System.out.println(nof4 + nof3 + nof2);
+
     solve(o, 4, o.numOfTeamOf4);
     solve(o, 3, o.numOfTeamOf3);
     solve(o, 2, o.numOfTeamOf2);
+
+    System.out.print(score);
 
 
   }
